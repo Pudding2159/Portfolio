b4524b36a34bc8cdde60ec98bdf05fdd68c74b5c
Mykyta Makhorin <nikitos2159@gmail.com>
2024-03-17 21:30:00 +0100

    add new custom cursor components

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,7 @@
-"use client"; 
+"use client";
 import React from 'react'
 import '@styles/globals.css';
+import CustomCursor from '@components/Cursors';
 import Nav from 'components/Nav';
 import NavbarM from 'components/NavbarMenu';
 import Main from "components/Main";
@@ -9,12 +10,17 @@ import Skills from 'components/Skills';
 import Projects from 'components/Projects';
 import Contact from 'components/Contact';
 import { Reveal } from '@components/Reveal';
+
 const Home = ({ children }) => {
     return (
         <div>
 
+            <div className = "hidden md:flex">
+                <CustomCursor />
+            </div>
             <NavbarM />
 
+            <div class="circle"></div>
             <div className="main">
                 <div className="gradient" />
             </div>
